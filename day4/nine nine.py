da38a82928356b49c342913d33ed3bd72ede4483
leolu <luqing28_js@sina.com>
2019-11-08 17:43:52 +0800

    nine nine test 2

--- day4/nine nine.py
+++ day4/nine nine.py
@@ -1,3 +1,16 @@
 """
 九九乘法表
 """
+value = 0
+for i in range(1, 10):
+    for b in range(1, 10):
+        value = i * b
+        print('%dx%d=%d' % (i, b, value), end='\t')
+    print()
+
+
+'第二种写法（展现的表格式不一样）'
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('%d*%d=%d' % (i, j, i * j), end='\t')
+    print()
