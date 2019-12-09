0e11386e4446b1d8ebd3d30d759a308b54b21f97
leolu <luqing28_js@sina.com>
2019-12-09 21:10:03 +0800

    triangle change 2

--- day4/triangle.py
+++ day4/triangle.py
@@ -8,15 +8,11 @@ date:2019.12.9
 ****
 *****
 
-
-
     *
    **
   ***
  ****
 *****
-
-
 
     *
    ***
@@ -27,7 +23,8 @@ date:2019.12.9
 
 print('this is a programme show different triangle')
 print('first triangle')
-for i in range(1, 6):
+row = int(input('please input your row number:'))
+for i in range(1, row + 1):
     for j in range(0, i):
         print('*', end='')
     print()
@@ -35,9 +32,20 @@ for i in range(1, 6):
 print()
 print('second triangle')
 
-for i in range(1, 6):
-    for j in range(5, 0, -1):
+for i in range(1, row + 1):
+    for j in range(row, 0, -1):
         if j > i:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
+
+print()
+print('third triangle')
+
+for i in range(0, row):
+    for j in range(0, row * 2):
+        if j < row - i or j > row + i:
             print(' ', end='')
         else:
             print('*', end='')
